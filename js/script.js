17c377789a3e4f4315a03384bca661190a8c41ad
schangwv <32345613+schangwv@users.noreply.github.com>
2018-04-14 13:39:09 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// put locations in a vector-like data structure
+
+// randomize the locations
+
+// randomly output a location and store it in a location variable
+
+// put activities in a vector-like data structure
+
+// randomly output an activity and store it in an activity variable
