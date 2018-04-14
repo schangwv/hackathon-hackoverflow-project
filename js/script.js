0ef03fa32751c55ac55c83ff5f5b88f31da2e454
schangwv <32345613+schangwv@users.noreply.github.com>
2018-04-14 14:03:02 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 // put locations in a vector-like data structure
+var locations = [Hoover Tower, Herbert Hoover Memorial Exhibit Pavilion, Anderson Collection, Cantor Arts Center, Bing Concert Hall, Dinkelspiel Auditorium, Memorial Auditorium, Memorial Church, Jasper Ridge Biological Preserve, Oval];
 
-// randomize the locations
 
 // randomly output a location and store it in a location variable
 
 // put activities in a vector-like data structure
 
 // randomly output an activity and store it in an activity variable
+
+// print out activity + location
+document.getElementById("location").innerHTML = location
+document.getElementById("activity").innerHTML = activity
