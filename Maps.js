57794a97ed4cdd4001c54dd1f95556c008f2c77d
chloebarreau <artdynamite@gmail.com>
2018-04-14 16:18:45 -0700

    Add files via upload

---
+++ Maps.js
@@ -0,0 +1,28 @@
+"use strict";
+
+ // randomizes types of activities or locations and their associated details
+
+function Locations() {
+	var locationMap = {
+  "Sports": ["Cardinal Wrestling Club", "Stanford Climbing Walls"],
+  "Stanford Anderson Collection": ["Make a meme from an artwork", "Convince a stranger of a story about an artwork of your chosing", "Visit the security guard's colleciton favorite"],
+	"Boxing": ["Urban Combat", "Studio Kicks"],
+	"Museum": ["Computer History Museum", "Tech Museum of Innovation", "Intel Museum"],
+  "Drag Show": ["Aunt Charlie's Lounge", "The Starlight Room", "Oasis"]
+	};
+
+	var keys = Array.from(Object.keys(locationMap));
+	var what = document.getElementById("what");
+	var where = document.getElementById("where");
+	console.log(keys);
+	var location = keys[Math.floor(Math.random()*keys.length)]
+  what.innerHTML = location;
+	var value = locationMap[location];
+	where.innerHTML = value[Math.floor(Math.random()*value.length)];
+	console.log(Math.floor(Math.random()*value.length));
+	console.log(locationMap[location]);
+}
+
+function refresh() {
+    location.reload();
+}
